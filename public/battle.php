c690dd281510f986c8764e789d7a58dfaed390ef
KID the Euforia <kid0725+github@gmail.com>
2017-05-25 20:23:25 +0900

    バトル展開を充実させた

--- public/battle.php
+++ public/battle.php
@@ -13,73 +13,84 @@ if (is_null($player_id)) {
     header('Location: /index.php');
     return;
 }
+$player = PlayerQuery::create()->findPK($player_id);
 
 if (isset($_POST['enemy_player_id'])) {
-    $player = PlayerQuery::create()->findPK($player_id);
     $enemy_player = PlayerQuery::create()->findPK($_POST['enemy_player_id']);
 
     $player_points = $player->getPoints();
     $enemy_player_points = $enemy_player->getPoints();
 
-    echo '<pre>';
-    echo 'before battle<br>';
-    echo 'player_points: ';
-    var_dump($player_points);
-    echo 'enemy_player_points: ';
-    var_dump($enemy_player_points);
-    echo '</pre>';
+    echo '相手のステータス↓';
+    echo '<ul>';
+    foreach ($enemy_player_points as $key => $point) {
+        printf('<li>%s: %d</li>', $key, $point);
+    }
+    echo '</ul>';
 
     $player_deck = $player->getPlayerDecks()->getFirst();
     $enemy_player_deck = $enemy_player->getPlayerDecks()->getFirst();
 
     $property_names = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
+    $nb_phase = 1;
+    echo 'バトル経過↓';
+    echo '<ul>';
     foreach ($property_names as $property_name) {
-        $method_name = "get${property_name}PropriumId";
-        $player_proprium_id = $player_deck->$method_name();
-        $enemy_player_proprium_id = $enemy_player_deck->$method_name();
+        $method_name = "getPropriumRelatedBy${property_name}PropriumId";
+        $player_proprium = $player_deck->$method_name();
+        $enemy_player_proprium = $enemy_player_deck->$method_name();
+
+        echo "<li>phase${nb_phase}</li>";
+        $nb_phase++;
+        echo '<ul>';
+        printf('<li>自分: %s vs. 相手 %s</li>', $player_proprium->getName(), $enemy_player_proprium->getName());
 
         // じゃんけん
-        $diff = $player_proprium_id - $enemy_player_proprium_id;
+        $diff = $player_proprium->getId() - $enemy_player_proprium->getId();
         switch ($diff) {
             case -1:
             case 2:
                 $damage_point = $player_points['attack_point'] - $enemy_player_points['defense_point'];
-                if (0 < $damage_point) {
-                    $enemy_player_points['hit_point'] -= $damage_point;
+                if ($damage_point < 0) {
+                    $damage_point = 0;
                 }
+                $enemy_player_points['hit_point'] -= $damage_point;
+                printf('<li>自分の攻撃！相手に%dポイントのダメージ！</li>', $damage_point);
                 break;
             case -2:
             case 1:
                 $damage_point = $enemy_player_points['attack_point'] - $player_points['defense_point'];
-                if (0 < $damage_point) {
-                    $player_points['hit_point'] -= $damage_point;
+                if ($damage_point < 0) {
+                    $damage_point = 0;
                 }
+                $player_points['hit_point'] -= $damage_point;
+                printf('<li>相手の攻撃！自分に%dポイントのダメージ！</li>', $damage_point);
                 break;
             default:
                 $damage_point = $player_points['attack_point'] - $enemy_player_points['defense_point'];
-                if (0 < $damage_point) {
-                    $enemy_player_points['hit_point'] -= round($damage_point * 0.5);
+                if ($damage_point < 0) {
+                    $damage_point = 0;
                 }
+                $enemy_player_points['hit_point'] -= $damage_point;
+                printf('<li>自分の攻撃！相手に%dポイントのダメージ！</li>', $damage_point);
+
                 $damage_point = $enemy_player_points['attack_point'] - $player_points['defense_point'];
-                if (0 < $damage_point) {
-                    $player_points['hit_point'] -= round($damage_point * 0.5);
+                if ($damage_point < 0) {
+                    $damage_point = 0;
                 }
+                $player_points['hit_point'] -= $damage_point;
+                printf('<li>相手の攻撃！自分に%dポイントのダメージ！</li>', $damage_point);
                 break;
         }
+        printf('<li>[現在のHP] 自分: %d / 相手: %d</li>', $player_points['hit_point'], $enemy_player_points['hit_point']);
+        echo '</ul>';
 
         // どちらかのHPが0以下であれば即時中止
         if ($player_points['hit_point'] <= 0 || $enemy_player_points['hit_point'] <= 0) {
             break;
         }
     }
-
-    echo '<pre>';
-    echo 'after battle<br>';
-    echo 'player_points: ';
-    var_dump($player_points);
-    echo 'enemy_player_points: ';
-    var_dump($enemy_player_points);
-    echo '</pre>';
+    echo '</ul>';
 
     $player_battle_log = new PlayerBattleLog();
     $player_battle_log->fromArray([
@@ -94,7 +105,11 @@ if (isset($_POST['enemy_player_id'])) {
         'challenged' => false,
     ], PlayerBattleLogTableMap::TYPE_FIELDNAME);
 
-    if ($player_points['hit_point'] <= 0 || $player_points['hit_point'] < $enemy_player_points['hit_point']) {
+    if ($player_points['hit_point'] <= 0 && $enemy_player_points['hit_point'] <= 0) {
+        echo '引き分け<br>';
+        $player_battle_log->setResult(PlayerBattleLog::RESULT_DRAW);
+        $enemy_player_battle_log->setResult(PlayerBattleLog::RESULT_DRAW);
+    } elseif ($player_points['hit_point'] <= 0 || $player_points['hit_point'] < $enemy_player_points['hit_point']) {
         echo '負けた...<br>';
         $player_battle_log->setResult(PlayerBattleLog::RESULT_LOSE);
         $enemy_player_battle_log->setResult(PlayerBattleLog::RESULT_WIN);
@@ -103,9 +118,7 @@ if (isset($_POST['enemy_player_id'])) {
         $player_battle_log->setResult(PlayerBattleLog::RESULT_WIN);
         $enemy_player_battle_log->setResult(PlayerBattleLog::RESULT_LOSE);
     } else {
-        echo '引き分け<br>';
-        $player_battle_log->setResult(PlayerBattleLog::RESULT_DRAW);
-        $enemy_player_battle_log->setResult(PlayerBattleLog::RESULT_DRAW);
+        echo '謎<br>';
     }
 
     $connection = Propel::getWriteConnection(PlayerBattleLogTableMap::DATABASE_NAME);
@@ -119,7 +132,17 @@ if (isset($_POST['enemy_player_id'])) {
     }
 }
 ?>
-デッキ構築した人としかバトルできません。
+デッキ構築した人としかバトルできません。<br>
+自分のステータス↓
+<ul>
+<?php
+$points = $player->getPoints();
+foreach ($points as $key => $point) {
+    printf('<li>%s: %d</li>', $key, $point);
+}
+?>
+</ul>
+
 <form method="post">
 <select name="enemy_player_id">
 <?php
